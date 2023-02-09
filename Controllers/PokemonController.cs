6f8b574c8dbfdea3ee5bccec00d1ebe33091cf18
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-02-09 16:36:41 +0100

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.DTO;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IMapper _mapper;
+
+        public PokemonController(IPokemonRepository pokemonRepository,IMapper mapper)
+        {
+            _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(200,Type=typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons =_mapper.Map<List<PokemonDto>>( _pokemonRepository.GetPokemons());
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(200)]
+        public IActionResult GetPokemon(int pokeid)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeid))
+                return NotFound();
+
+            var pokemon =_mapper.Map<Pokemon>( _pokemonRepository.GetPokemon(pokeid));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(200)]
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound();
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(rating);
+        }
+    }
+}
