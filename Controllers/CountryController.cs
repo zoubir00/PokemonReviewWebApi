6a82af10cb9e4fd7f24df2ab68b7384a1a1eb038
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-02-20 11:54:14 +0100

    Get Methods for country

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public CountryController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Country>))]
+        public IActionResult Index()
+        {
+            var countries = _countryRepository.GetCountries();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(countries);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCountry(int id)
+        {
+            if (!_countryRepository.CountryExists(id))
+                return NotFound();
+            var country = _countryRepository.GetCountry(id);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(country);
+        }
+
+        [HttpGet("Country/{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCountryByOwner(int ownerId)
+        {
+            var Country = _countryRepository.GetCountryByOwner(ownerId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(Country);
+        }
+    }
+}
