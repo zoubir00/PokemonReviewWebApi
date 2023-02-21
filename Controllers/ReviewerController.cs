216ccf1d8a78733bc6c992bc81d7d144fc4c737f
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-02-21 15:56:19 +0100

    Create Reviewer get methods

---
+++ Controllers/ReviewerController.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.DTO;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewerController : Controller
+    {
+        private IReviewerRepository _reviewerRepository;
+        private IMapper _mapper;
+
+        public ReviewerController(IReviewerRepository reviewerRepository, IMapper mapper)
+        {
+            _reviewerRepository = reviewerRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Reviewer>))]
+        public IActionResult GetReviewers()
+        {
+            var reviewers = _mapper.Map<List<ReviewerDto>>(_reviewerRepository.GetReviewers());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(reviewers);
+        }
+
+        [HttpGet("{reviewerId}")]
+        [ProducesResponseType(200, Type = typeof(Reviewer))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int reviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+
+            var reviewer = _mapper.Map<ReviewerDto>(_reviewerRepository.GetReviewer(reviewerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(reviewer);
+        }
+
+        [HttpGet("{reviewerId}/reviews")]
+        public IActionResult GetReviewsByAReviewer(int reviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(reviewerId))
+                return NotFound();
+
+            var reviews = _mapper.Map<List<ReviewDto>>(
+                _reviewerRepository.GetReviewsByReviewer(reviewerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(reviews);
+        }
+    }
+}
